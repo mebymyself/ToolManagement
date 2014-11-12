64855f12b172ab21a1b7c3003ba4b6e91d55a0f0
Lazigerbill <chaukbill@gmail.com>
2014-11-12 14:06:14 -0500

    employee search function integrated

--- app/assets/javascripts/employees.js
+++ app/assets/javascripts/employees.js
@@ -15,27 +15,64 @@ var ready;
 ready = function() {  
 	$('#search-employee').click(function(){
 		event.preventDefault();
-		var searchValue = $('#employee_search_box').val()
-
-		$.getJSON('/employees?search=' + searchValue)
-			.done(function(data){
-				for(i=0; i < data.length; i++){
-					console.log(data[i].avatar_url);
-					$('#employee_barcode').text(data[i].barcode)
-					$('#employee_pic').attr('src', data[i].avatar_url);
-					$('#employee_first_name').text(data[i].first_name);
-					$('#employee_last_name').text(data[i].last_name);
-					$('#employee_updated_at').text(data[i].updated_at);
-				}
-		})
+		employeeModal();
 	});
 
 	$('.cancel_employee').click(function(){
-		$('#employee_search_box').val("");
+		employeeSearchReset();
+	})
+
+	$('.invalid_search').click(function(){
+		employeeSearchReset();
+	})
+
+	$('#confirm_employee').click(function(){
+		$('#confirm_employee').show();
+		$('.cancel_employee').show();
+		$('.invalid_search').show();
 	})
 }
 $(document).ready(ready);
 $(document).on('page:load', ready);
+
+
+function employeeModal(){
+	var searchValue = $('#issuance_incoming_employee_barcode').val()
+
+	$.getJSON('/employees?search=' + searchValue)
+		.done(function(data){
+			if (data.length !== 1) {
+				$('#employee_barcode').text("Record not found");
+				$('#employee_pic').attr('src', "/assets/unavailable.png");
+				$('#employee_first_name').text("Record not found");
+				$('#employee_last_name').text("Record not found");
+				$('#employee_updated_at').text("Record not found");	
+				$('#confirm_employee').hide();
+				$('.cancel_employee').hide();
+			} else {
+				$('#issuance_incoming_employee_barcode').val(data[0].barcode);
+				$('#employee_barcode').text(data[0].barcode);
+				$('#employee_pic').attr('src', data[0].avatar_url);
+				$('#employee_first_name').text(data[0].first_name);
+				$('#employee_last_name').text(data[0].last_name);
+				$('#employee_updated_at').text(data[0].updated_at);	
+				$('.invalid_search').hide();
+
+			};
+	})
+}
+
+function employeeSearchReset(){
+	$('#issuance_incoming_employee_barcode').val("");
+	$('#employee_barcode').text("")
+	$('#employee_pic').attr('src', "");
+	$('#employee_first_name').text("");
+	$('#employee_last_name').text("");
+	$('#employee_updated_at').text("");
+	$('#confirm_employee').show();
+	$('.cancel_employee').show();
+	$('.invalid_search').show();
+}
 
 // Need clear modal function
 // Need clear text field function
